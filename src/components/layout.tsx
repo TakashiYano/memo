2d7f6f17836fad74dc6e142730ce50907542e6f3
TakashiYano <0tp6324w3261z0v@gmail.com>
2021-06-30 09:19:21 +0000

    ✨ Add tailwindcss.

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -6,7 +6,7 @@ export const Layout: VFC<{ children: ReactNode }> = (props) => {
   return (
     <>
       <Header />
-      <main>{props.children}</main>
+      <main className="bg-red-100">{props.children}</main>
       <Footer />
     </>
   );
