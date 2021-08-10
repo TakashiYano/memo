18ef281b5781f16782f3e9a090f75e64a38a7791
TakashiYano <0tp6324w3261z0v@gmail.com>
2021-08-10 12:52:15 +0000

    ✨ MSW init setting.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,10 @@ import "tailwindcss/tailwind.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 
+if (process.env.NODE_ENV === "development") {
+  require("src/mocks");
+}
+
 const App = (props: AppProps) => {
   return (
     <ThemeProvider attribute="class">
