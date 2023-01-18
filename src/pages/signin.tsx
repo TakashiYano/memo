2656008458e3ac006a8ba39226f000e984f7d9a4
yanot <0tp6324w3261z0v@gmail.com>
2023-01-18 18:15:07 +0900

    💄 add signin

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,5 +1,26 @@
-const SignIn = () => {
-  return <div>SignIn</div>;
+import Head from "next/head";
+import { AppleIcon } from "src/components/icon/AppleIcon";
+import { GoogleIcon } from "src/components/icon/GoogleIcon";
+import { Button } from "src/components/share/Button";
+
+const Signin: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Signin</title>;
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="bg-gray-200 w-screen h-screen">
+        <div className="flex justify-center mb-16">Memo</div>
+        <Button id="googleButton" color="white" StartIcon={GoogleIcon} size="large">
+          Googleでアカウント作成
+        </Button>
+        <Button id="appleButton" color="black" StartIcon={AppleIcon} size="large">
+          Appleでアカウント作成
+        </Button>
+      </div>
+    </div>
+  );
 };
 
-export default SignIn;
+export default Signin;
