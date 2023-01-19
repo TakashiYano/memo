d1b7f806f0f33133f1d279be3f9c3ba871f1b9e9
yanot <0tp6324w3261z0v@gmail.com>
2023-01-19 18:21:13 +0900

    🐛 Buttonコンポーネント異常終了対応

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -5,6 +5,14 @@ import { GoogleIcon } from "src/components/icon/GoogleIcon";
 import { Button } from "src/components/share/Button";
 
 const Signin: FC = () => {
+  // onClickイベントを渡さないとButtonコンポーネント側でlinkと
+  // 判断されるため処理は何もしないけど、渡す
+  const handleGoogleRegister = () => {
+    return null;
+  };
+  const handleAppleRegister = () => {
+    return null;
+  };
   return (
     <div>
       <Head>
@@ -14,10 +22,28 @@ const Signin: FC = () => {
       <div className="bg-gray-200 w-screen h-screen">
         <div className="h-screen flex flex-col justify-center">
           <div className="flex justify-center mb-16">Memo</div>
-          <Button type="button" id="googleButton" color="white" StartIcon={GoogleIcon} size="large" className="w-80">
+          <Button
+            type="button"
+            id="googleButton"
+            color="white"
+            StartIcon={GoogleIcon}
+            size="large"
+            className="w-80"
+            onClick={handleGoogleRegister}
+            onKeyDown={handleGoogleRegister}
+          >
             Googleでアカウント作成
           </Button>
-          <Button type="button" id="appleButton" color="black" StartIcon={AppleIcon} size="large" className="w-80">
+          <Button
+            type="button"
+            id="appleButton"
+            color="black"
+            StartIcon={AppleIcon}
+            size="large"
+            className="w-80"
+            onClick={handleAppleRegister}
+            onKeyDown={handleAppleRegister}
+          >
             Appleでアカウント作成
           </Button>
         </div>
