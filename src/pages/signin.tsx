95a2cfd60a0df4b9d8034a01adcaefc7785177b3
yanot <0tp6324w3261z0v@gmail.com>
2023-01-19 18:17:40 +0900

    🩹

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -14,10 +14,10 @@ const Signin: FC = () => {
       <div className="bg-gray-200 w-screen h-screen">
         <div className="h-screen flex flex-col justify-center">
           <div className="flex justify-center mb-16">Memo</div>
-          <Button id="googleButton" color="white" StartIcon={GoogleIcon} size="large" className="w-80">
+          <Button type="button" id="googleButton" color="white" StartIcon={GoogleIcon} size="large" className="w-80">
             Googleでアカウント作成
           </Button>
-          <Button id="appleButton" color="black" StartIcon={AppleIcon} size="large" className="w-80">
+          <Button type="button" id="appleButton" color="black" StartIcon={AppleIcon} size="large" className="w-80">
             Appleでアカウント作成
           </Button>
         </div>
