db0d2dacb8c4fc8d634a8a79aa816ba033a48f36
yanot <0tp6324w3261z0v@gmail.com>
2023-01-25 15:36:12 +0900

    🐛 useSWRを使用するように修正

--- src/pages/users/[userId].tsx
+++ src/pages/users/[userId].tsx
@@ -1,37 +1,47 @@
 import type { NextPage } from "next";
-import { useEffect, useState } from "react";
+import { SearchIcon } from "src/components/icon/SearchIcon";
 import { Avatar } from "src/components/shared/Avatar";
 import { Button } from "src/components/shared/Button";
+import { InputText } from "src/components/shared/InputText";
 import { MemoCard } from "src/components/users/MemoCard";
-import { UserHeader } from "src/components/users/userHeader";
 import { EXAMPLE_USER_01 } from "src/models/user";
 import type { ListNote } from "src/types/types";
-
-import { fetcher, userUrl } from "../../lib/api";
+import useSWR from "swr";
 
 // **********************************
 // ユーザ情報はログイン時に取得している想定のため、一旦固定値にする
-// Google認証でもApple認証でもOAuth2.0ならトークンでユーザ情報取得しているはずで
+// Google認証でもApple認証でもOAuth2.0ならトークンでユーザ情報取得しているはず
 const user = EXAMPLE_USER_01;
 
 const User: NextPage = () => {
-  const [listNote, setListNote] = useState<ListNote[]>([]);
-
-  useEffect(() => {
-    const fetchBootLoader = async () => {
-      const listNote = await fetcher<ListNote[]>(`${userUrl}/${user.id}/notes/`);
-      setListNote(listNote);
-    };
-    fetchBootLoader();
-  }, []);
+  const { data: listNote, error } = useSWR<ListNote[]>(`/users/${user.id}/notes`);
 
   return (
     <div className="flex flex-col overscroll-none h-screen">
       <header>
-        <UserHeader />
+        <div className="w-10/12 lg:w-auto mx-auto flex flex-col">
+          <div className="flex flex-row items-center">
+            <div className="flex-1">
+              <div className="flex lg:justify-end mr-4">Memo</div>
+            </div>
+            <div className="hidden lg:block flex-1 my-auto mx-16">
+              <InputText startIcon={<SearchIcon className="my-auto mr-2 w-6 h-6" />} placeholder="メモを検索する" />
+            </div>
+            <div className="flex-1">
+              <div className="flex flex-row justify-end lg:justify-start">
+                <div className="ml-4">
+                  <Button button>メモを書く</Button>
+                </div>
+                <div className="my-auto">
+                  <Avatar alt={user.name} src={user.avatarUrl} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </header>
-      <div className="w-1/2 mx-auto">
-        <div className="flex flex-row">
+      <div className="w-10/12 lg:w-1/2 mx-auto">
+        <div className="flex flex-row my-4">
           <div className="flex flex-col">
             <Avatar alt={user.name} src={user.avatarUrl} size="medium" />
           </div>
@@ -42,11 +52,17 @@ const User: NextPage = () => {
             </Button>
           </div>
         </div>
-        <div className="w-full flex flex-col h-full">
-          {listNote.map((note: ListNote) => {
-            return <MemoCard key={note.id} note={note} />;
-          })}
+        <div className="block lg:hidden">
+          <InputText startIcon={<SearchIcon className="my-auto mr-2 w-6 h-6" />} placeholder="メモを検索する" />
         </div>
+        {error ? <div>メモが登録されていません</div> : null}
+        {listNote ? (
+          <div className="w-full flex flex-col h-full">
+            {listNote.map((note: ListNote) => {
+              return <MemoCard key={note.id} note={note} />;
+            })}
+          </div>
+        ) : null}
       </div>
     </div>
   );
