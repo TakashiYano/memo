8c00d176a741af83d43c78fcbd6898f786ecc868
yanot <0tp6324w3261z0v@gmail.com>
2023-01-25 15:01:44 +0900

    🩹 mswで型の定義間違いの修正

--- src/models/note.ts
+++ src/models/note.ts
@@ -2,14 +2,14 @@ export type Note = {
   id: string;
   content: string;
   public: boolean;
-  data: string;
+  date: string;
 };
 
 export type ListNote = {
   id: string;
   excerpt: string;
   public: boolean;
-  data: string;
+  date: string;
 };
 
 export const EXAMPLE_NOTE: Note = {
@@ -20,7 +20,7 @@ export const EXAMPLE_NOTE: Note = {
   ・At architecto modi neque, laudantium quidem dicta ipsam voluptates error magnam, cum quibusdam accusamus officiis ex. Architecto minima sequi doloribus veritatis ipsum.
   `,
   public: false,
-  data: "2021/3/1",
+  date: "2021/3/1",
 };
 
 export const EXAMPLE_MY_NOTE_LIST: ListNote[] = [
@@ -28,31 +28,31 @@ export const EXAMPLE_MY_NOTE_LIST: ListNote[] = [
     id: "5",
     excerpt: `Lorem ipsum dolor, sit amet consectetur adipisicing elit`,
     public: false,
-    data: "2020/10/11",
+    date: "2020/10/11",
   },
   {
     id: "4",
     excerpt: `Lorem ipsum dolor, sit amet consectetur adipisicing elit`,
     public: true,
-    data: "2020/12/11",
+    date: "2020/12/11",
   },
   {
     id: "3",
     excerpt: `Lorem ipsum dolor, sit amet consectetur adipisicing elit`,
     public: false,
-    data: "2021/2/1",
+    date: "2021/2/1",
   },
   {
     id: "2",
     excerpt: `Lorem ipsum dolor, sit amet consectetur adipisicing elit`,
     public: false,
-    data: "2021/3/1",
+    date: "2021/3/1",
   },
   {
     id: "1",
     excerpt: `Lorem ipsum dolor, sit amet consectetur adipisicing elit`,
     public: true,
-    data: "2021/3/20",
+    date: "2021/3/20",
   },
 ];
 
@@ -61,30 +61,30 @@ export const EXAMPLE_OTHER_USER_NOTE_LIST: ListNote[] = [
     id: "5",
     excerpt: `Lorem ipsum dolor, sit amet consectetur adipisicing elit`,
     public: false,
-    data: "2020/10/11",
+    date: "2020/10/11",
   },
   {
     id: "4",
     excerpt: `Lorem ipsum dolor, sit amet consectetur adipisicing elit`,
     public: true,
-    data: "2020/10/21",
+    date: "2020/10/21",
   },
   {
     id: "3",
     excerpt: `Lorem ipsum dolor, sit amet consectetur adipisicing elit`,
     public: true,
-    data: "2020/12/11",
+    date: "2020/12/11",
   },
   {
     id: "2",
     excerpt: `Lorem ipsum dolor, sit amet consectetur adipisicing elit`,
     public: false,
-    data: "2021/01/15",
+    date: "2021/01/15",
   },
   {
     id: "1",
     excerpt: `Lorem ipsum dolor, sit amet consectetur adipisicing elit`,
     public: true,
-    data: "2021/02/20",
+    date: "2021/02/20",
   },
 ];
