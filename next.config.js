d1209b814f526915d33ed33034a9988f2fe8367a
yanot <0tp6324w3261z0v@gmail.com>
2023-01-25 16:23:27 +0900

    ⬆️

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   i18n: { locales: ["ja"], defaultLocale: "ja" },
+  future: { strictPostcssConfiguration: true },
   reactStrictMode: true,
   typescript: { ignoreDevErrors: true },
   poweredByHeader: false,
