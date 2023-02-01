a8fb8eb68f928279137a3dcd766a63f2cc45ccd4
yanot <0tp6324w3261z0v@gmail.com>
2023-02-01 17:01:22 +0900

    🌐 next.config.jsを使ったlangの設定、_documentを削除

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   i18n: { locales: ["ja"], defaultLocale: "ja" },
-  future: { strictPostcssConfiguration: true },
   reactStrictMode: true,
   typescript: { ignoreDevErrors: true },
   poweredByHeader: false,
