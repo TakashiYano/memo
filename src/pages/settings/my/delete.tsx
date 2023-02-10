02b2a2fae6b6e92486a3dd2d3184f86dea3669db
yanot <0tp6324w3261z0v@gmail.com>
2023-02-10 17:39:14 +0900

    🔥 不必要なコードを削除

--- src/pages/settings/my/delete.tsx
+++ src/pages/settings/my/delete.tsx
@@ -5,32 +5,29 @@ import { RecursiveList } from "src/components/shared/List";
 const SettingsAccount: NextPage = () => {
   return (
     <Layout left="back" center="account">
-      <div className="mx-auto max-w-screen-sm">
-        <h1 className="text-xl font-bold">データの削除</h1>
-        <div className="mt-8">
-          <RecursiveList
-            list={[
-              {
-                title: "サービスの削除",
-                items: [
-                  {
-                    label: "Memoを削除",
-                    button: {
-                      label: (
-                        <div className="py-2 px-5 text-sm font-bold text-red-500 bg-gray-200 rounded-full">
-                          削除する
-                        </div>
-                      ),
-                      onClick: () => {
-                        alert(123);
-                      },
+      <h1 className="text-xl font-bold">データの削除</h1>
+
+      <div className="mt-8">
+        <RecursiveList
+          list={[
+            {
+              title: "サービスの削除",
+              items: [
+                {
+                  label: "Memoを削除",
+                  button: {
+                    label: (
+                      <div className="py-2 px-5 text-sm font-bold text-red-500 bg-gray-200 rounded-full">削除する</div>
+                    ),
+                    onClick: () => {
+                      alert(123);
                     },
                   },
-                ],
-              },
-            ]}
-          />
-        </div>
+                },
+              ],
+            },
+          ]}
+        />
       </div>
     </Layout>
   );
