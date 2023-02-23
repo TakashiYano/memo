509b7221f44aec42e661da1cc1641893ede912a8
yanot <0tp6324w3261z0v@gmail.com>
2023-02-23 18:01:39 +0900

    🔧 tailwindcss v3.0対応

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require("tailwindcss/colors");
 
 module.exports = {
   mode: "jit",
-  purge: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
     extend: {
