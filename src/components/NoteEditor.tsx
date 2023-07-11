48f9b898792a01d726ed1a4de2e4f6a26e0f6307
yanot <0tp6324w3261z0v@gmail.com>
2023-07-11 16:21:03 +0900

    ⚡️ エディタにdebounce処理を追加

--- src/components/NoteEditor.tsx
+++ src/components/NoteEditor.tsx
@@ -1,25 +1,39 @@
 import type { ChangeEvent } from "react";
-import { useCallback, useState } from "react";
+import { useCallback } from "react";
+import { toast } from "react-hot-toast";
 import TextareaAutosize from "react-textarea-autosize";
 import type { NoteType } from "src/type/types";
+import { useDebouncedCallback } from "use-debounce";
 
 export const NoteEditor = (props: NoteType) => {
-  const [content, setContent] = useState(props.content);
+  const debounced = useDebouncedCallback(async (value) => {
+    try {
+      // TODO: メモ更新処理を追加
+      alert(value.trim());
+    } catch (error) {
+      toast.error("エラーが発生したため保存に失敗しました。時間を空けてから再度お試しください。");
+      console.error(error);
+    }
+  }, 1500);
 
-  const handleChangeContent = useCallback(async (event: ChangeEvent<HTMLTextAreaElement>) => {
-    setContent(event.currentTarget.value);
-  }, []);
+  const handleChange = useCallback(
+    (e: ChangeEvent<HTMLTextAreaElement>) => {
+      return debounced(e.target.value);
+    },
+    [debounced]
+  );
 
   return (
-    <label htmlFor="memo" className="flex h-[calc(100vh-168px)] cursor-text sm:h-[calc(100vh-192px)]">
+    <label htmlFor="memo" className="block">
       <TextareaAutosize
         id="memo"
         style={{ caretColor: "#3B82F6" }}
         className="w-full resize-none border-none bg-transparent text-lg focus:ring-0 sm:text-xl"
-        value={content}
-        onChange={handleChangeContent}
+        defaultValue={props.content}
+        onChange={handleChange}
         placeholder="メモを入力する"
         autoComplete="off"
+        minRows={16}
       />
     </label>
   );
