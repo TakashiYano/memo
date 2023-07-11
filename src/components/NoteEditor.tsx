ce8137ebed114c095021e1ab39741fed9901d76a
yanot <0tp6324w3261z0v@gmail.com>
2023-07-11 16:37:19 +0900

    ⚡️ テキストエリアのフォーカスが外れたタイミングで保存する機能の実装

--- src/components/NoteEditor.tsx
+++ src/components/NoteEditor.tsx
@@ -1,15 +1,24 @@
+import { useRouter } from "next/router";
 import type { ChangeEvent } from "react";
-import { useCallback } from "react";
+import { useCallback, useEffect, useRef } from "react";
 import { toast } from "react-hot-toast";
 import TextareaAutosize from "react-textarea-autosize";
 import type { NoteType } from "src/type/types";
 import { useDebouncedCallback } from "use-debounce";
 
 export const NoteEditor = (props: NoteType) => {
-  const debounced = useDebouncedCallback(async (value) => {
+  const ref = useRef<HTMLTextAreaElement>(null);
+  const router = useRouter();
+
+  const saveNote = useCallback(async (value: string) => {
+    // TODO: メモ更新処理を追加
+    // eslint-disable-next-line no-console
+    console.log(value.trim());
+  }, []);
+
+  const debounced = useDebouncedCallback(async (value: string) => {
     try {
-      // TODO: メモ更新処理を追加
-      alert(value.trim());
+      await saveNote(value);
     } catch (error) {
       toast.error("エラーが発生したため保存に失敗しました。時間を空けてから再度お試しください。");
       console.error(error);
@@ -23,14 +32,31 @@ export const NoteEditor = (props: NoteType) => {
     [debounced]
   );
 
+  const handleBlur = useCallback(async () => {
+    if (ref.current?.value) {
+      await saveNote(ref.current.value);
+    }
+  }, [saveNote]);
+
+  useEffect(() => {
+    router.beforePopState(({ url }) => {
+      if (ref.current?.value && url === "/") {
+        saveNote(ref.current.value);
+      }
+      return true;
+    });
+  }, [router, saveNote]);
+
   return (
     <label htmlFor="memo" className="block">
       <TextareaAutosize
+        ref={ref}
         id="memo"
         style={{ caretColor: "#3B82F6" }}
         className="w-full resize-none border-none bg-transparent text-lg focus:ring-0 sm:text-xl"
         defaultValue={props.content}
         onChange={handleChange}
+        onBlur={handleBlur}
         placeholder="メモを入力する"
         autoComplete="off"
         minRows={16}
