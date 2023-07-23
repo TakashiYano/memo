1ec4a07ddd66c8af4b9cfa4b3573453bcdd3a90e
yanot <0tp6324w3261z0v@gmail.com>
2023-07-23 12:34:43 +0900

    🎨 メモ一覧の1行目・2行目の出し方のロジックを修正

--- src/components/NoteListItem.tsx
+++ src/components/NoteListItem.tsx
@@ -3,15 +3,10 @@ import type { FC } from "react";
 import { format_yyyyMd } from "src/lib/date";
 import type { ListNoteType } from "src/type/types";
 
-const CHUNK_SIZE = 120; // Number of characters per line
-
 // Get first and second line of string
 const getFirstAndSecondLine = (str: string) => {
   const [first, second] = str.split("\n").filter(Boolean);
-  if (first.length > CHUNK_SIZE) {
-    return [str.slice(0, CHUNK_SIZE / 2), str.slice(CHUNK_SIZE / 2, str.length)];
-  }
-  return [first, second];
+  return [first, second || "\u00A0"];
 };
 
 export const NoteListItem: FC<ListNoteType> = (props) => {
@@ -21,8 +16,16 @@ export const NoteListItem: FC<ListNoteType> = (props) => {
     <article>
       <Link href={`/memos/${props.id}`}>
         <a className="block w-full rounded-xl bg-gray-100 px-4 py-3 shadow dark:bg-gray-700 sm:px-6">
-          <h1 className="truncate text-sm font-bold sm:text-base">{first}</h1>
-          <p className="mt-0.5 truncate text-sm">{second || "\u00A0"}</p>
+          {first.length > 120 ? (
+            <div className="line-clamp-2">
+              <h1 className="text-sm leading-relaxed first-line:font-bold sm:first-line:text-base">{props.excerpt}</h1>
+            </div>
+          ) : (
+            <div>
+              <h1 className="truncate text-sm font-bold leading-relaxed sm:text-base">{first}</h1>
+              <p className="truncate text-sm leading-relaxed">{second}</p>
+            </div>
+          )}
           <div className="mt-4 flex h-6 items-end justify-between">
             <time className="space-x-4 text-sm font-bold tracking-wide text-gray-400">
               {format_yyyyMd(props.updatedOn)}
