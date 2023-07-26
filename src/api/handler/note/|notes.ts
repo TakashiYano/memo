7d8a511bf2bd9ee1fa26a11967b9f0261d865172
yanot <0tp6324w3261z0v@gmail.com>
2023-07-26 12:05:19 +0900

    ♻️  微修正

--- src/api/handler/note/|notes.ts
+++ src/api/handler/note/|notes.ts
@@ -1,6 +1,15 @@
 import { rest } from "msw";
 
+import { EXAMPLE_NOTE } from "./data";
+
 const endpoint = "/notes";
+
+/**
+ * @package 自分のメモ一覧を取得する
+ */
+export const getNotes = rest.get<string, never, { id: string }>(endpoint, (_req, res, ctx) => {
+  return res(ctx.delay(1000), ctx.status(201), ctx.json(EXAMPLE_NOTE));
+});
 
 /**
  * @package 新しいメモを作成する
