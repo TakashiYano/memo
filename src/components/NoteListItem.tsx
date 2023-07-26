a344f62946ddb7a6105082bd7cb86db2d4502adb
yanot <0tp6324w3261z0v@gmail.com>
2023-07-26 11:56:57 +0900

    🩹 微修正

--- src/components/NoteListItem.tsx
+++ src/components/NoteListItem.tsx
@@ -13,31 +13,29 @@ export const NoteListItem: FC<ListNoteType> = (props) => {
   const [first, second] = getFirstAndSecondLine(props.excerpt);
 
   return (
-    <article>
-      <Link href={`/memos/${props.id}`} legacyBehavior>
-        <a className="block w-full rounded-xl bg-gray-100 px-4 py-3 shadow dark:bg-gray-700 sm:px-6">
-          {first.length > 120 ? (
-            <div className="line-clamp-2">
-              <h1 className="text-sm leading-relaxed first-line:font-bold sm:first-line:text-base">{props.excerpt}</h1>
-            </div>
-          ) : (
-            <div>
-              <h1 className="truncate text-sm font-bold leading-relaxed sm:text-base">{first}</h1>
-              <p className="truncate text-sm leading-relaxed">{second}</p>
-            </div>
-          )}
-          <div className="mt-4 flex h-6 items-end justify-between">
-            <time className="space-x-4 text-sm font-bold tracking-wide text-gray-400">
-              {format_yyyyMd(props.updatedOn)}
-            </time>
-            {props.public ? (
-              <div className="grid h-full place-content-center rounded-full bg-orange-400 px-2.5 text-xs font-bold text-white">
-                ラベル
-              </div>
-            ) : null}
+    <Link href={`/memos/${props.id}`} legacyBehavior>
+      <a className="block w-full rounded-xl bg-gray-100 px-4 py-3 shadow dark:bg-gray-700 sm:px-6">
+        {first.length > 120 ? (
+          <div className="line-clamp-2">
+            <h1 className="text-sm leading-relaxed first-line:font-bold sm:first-line:text-base">{props.excerpt}</h1>
           </div>
-        </a>
-      </Link>
-    </article>
+        ) : (
+          <div>
+            <h1 className="truncate text-sm font-bold leading-relaxed sm:text-base">{first}</h1>
+            <p className="truncate text-sm leading-relaxed">{second}</p>
+          </div>
+        )}
+        <div className="mt-4 flex h-6 items-end justify-between">
+          <time className="space-x-4 text-sm font-bold tracking-wide text-gray-400">
+            {format_yyyyMd(props.updatedOn)}
+          </time>
+          {props.public ? (
+            <div className="grid h-full place-content-center rounded-full bg-orange-400 px-2.5 text-xs font-bold text-white">
+              ラベル
+            </div>
+          ) : null}
+        </div>
+      </a>
+    </Link>
   );
 };
