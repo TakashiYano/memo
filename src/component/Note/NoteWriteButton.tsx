de7b9759aa351fae3f583adb89535c085790e3ba
yanot <0tp6324w3261z0v@gmail.com>
2023-08-15 22:27:15 +0900

    🐛 メモをダブルクリックしたら複数メモが作成されるバグの修正

--- src/component/Note/NoteWriteButton.tsx
+++ src/component/Note/NoteWriteButton.tsx
@@ -1,22 +1,26 @@
 import { useRouter } from "next/router";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { Button } from "src/component/Button";
 
 /** @package */
 export const NoteWriteButton = () => {
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleCreateMemo = useCallback(async () => {
+    setIsLoading(true);
+
     try {
       // TODO:新規メモ作成処理の実装
       await router.push(`/memo/new`);
     } catch (error) {
       console.error(error);
     }
+    setIsLoading(false);
   }, [router]);
 
   return (
-    <Button key="write memo" variant="solid-blue" onClick={handleCreateMemo} className="h-10 px-4">
+    <Button key="write memo" className="h-10 px-4" variant="solid-blue" onClick={handleCreateMemo} disabled={isLoading}>
       メモを書く
     </Button>
   );
