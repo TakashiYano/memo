5a318274834c8225cd8d0447a7faa46e21ddd0a6
yanot <0tp6324w3261z0v@gmail.com>
2023-09-25 15:24:35 +0900

    💄 ヘッダーを微修正

--- src/app/(home)/_component/Note/NoteWriteButton.tsx
+++ src/app/(home)/_component/Note/NoteWriteButton.tsx
@@ -3,7 +3,10 @@
 import { useCallback, useState } from "react";
 import { useRouter } from "next/navigation";
 
+import { PencilSquareIcon } from "@heroicons/react/24/outline";
+
 import { Button } from "@/component/Button/Button";
+import { ICON_SIZE } from "@/lib/const/constants";
 
 export const NoteWriteButton = () => {
   const router = useRouter();
@@ -24,12 +27,12 @@ export const NoteWriteButton = () => {
   return (
     <Button
       key="write memo"
-      className="h-10 px-4"
-      variant="solid"
+      className={ICON_SIZE}
+      variant="ghost"
       onClick={handleCreateMemo}
       disabled={isLoading}
     >
-      メモを書く
+      <PencilSquareIcon className="h-5 w-5" />
     </Button>
   );
 };
--- src/app/(home)/_component/TopBar/Header.tsx
+++ src/app/(home)/_component/TopBar/Header.tsx
@@ -1,10 +1,8 @@
 import { type FC } from "react";
-import Link from "next/link";
 
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
 import { NoteWriteButton } from "@/app/(home)/_component/Note/NoteWriteButton";
-import { UserMenu } from "@/app/(home)/_component/TopBar/UserMenu";
 import { Anchor } from "@/component/Button/Anchor";
 import { ICON_SIZE } from "@/lib/const/constants";
 
@@ -19,15 +17,12 @@ export const Header: FC<HeaderProps> = (props) => {
         isHeaderNarrow ? "max-w-screen-sm" : "max-w-screen-lg"
       }`}
     >
-      <Link href="/" className="text-xl font-bold">
-        Memo
-      </Link>
+      <div className="text-xl font-bold">Memo</div>
 
       <div className="flex h-10 items-center space-x-2 sm:space-x-3">
         <Anchor href="/search" variant="ghost" className={ICON_SIZE}>
           <MagnifyingGlassIcon className="h-5 w-5" />
         </Anchor>
-        <UserMenu />
         <NoteWriteButton />
       </div>
     </header>
