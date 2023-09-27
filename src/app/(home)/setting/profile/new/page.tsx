b680179ee69ceb01b79123452765c43895e4dc27
yanot <0tp6324w3261z0v@gmail.com>
2023-09-27 18:01:28 +0900

    🩹

--- src/app/(home)/setting/profile/new/page.tsx
+++ src/app/(home)/setting/profile/new/page.tsx
@@ -1,7 +1,11 @@
 import { ProfileForm } from "@/app/(home)/setting/profile/_component/ProfileForm";
 
 const ProfileNew = () => {
-  return <ProfileForm />;
+  return (
+    <div className="px-4">
+      <ProfileForm />
+    </div>
+  );
 };
 
 export default ProfileNew;
