937753b522da1188306d066d140c30ff0e9113b6
yanot <0tp6324w3261z0v@gmail.com>
2023-09-29 22:10:51 +0900

    🚸 認証されていない場合のリダイレクト処理を追加

--- src/app/(auth)/forgot-password/page.tsx
+++ src/app/(auth)/forgot-password/page.tsx
@@ -1,6 +1,26 @@
-import { ForgotPassword } from "@/app/(auth)/forgot-password/_component/ForgotPassword";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-const ForgotPasswordPage = () => {
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+
+import { ForgotPassword } from "@/app/(auth)/forgot-password/_component/ForgotPassword";
+import { type Database } from "@/lib/supabase/type";
+
+const ForgotPasswordPage = async () => {
+  const supabase = createServerComponentClient<Database>({
+    cookies,
+  });
+
+  // セッションの取得
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  // 認証している場合、リダイレクト
+  if (session) {
+    redirect("/");
+  }
+
   return <ForgotPassword />;
 };
 
