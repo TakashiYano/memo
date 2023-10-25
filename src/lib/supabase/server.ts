7e3dc0656c4cda96a0d3bc7fd57c4e226bede044
yanot <0tp6324w3261z0v@gmail.com>
2023-10-25 22:12:07 +0900

    🩹 ビルド時に発生したエラーを修正

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -5,7 +5,10 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { type Database } from "@/lib/supabase/type";
 
 export const createClient = () => {
+  const cookieStore = cookies();
   return createServerComponentClient<Database>({
-    cookies,
+    cookies: () => {
+      return cookieStore;
+    },
   });
 };
