a6974fc389e86730f2d3b9653a46247fb7981f73
yanot <0tp6324w3261z0v@gmail.com>
2023-10-30 17:46:07 +0900

    🏷️ ラベルテーブルの追加に伴う型情報の追加

--- src/lib/supabase/type.ts
+++ src/lib/supabase/type.ts
@@ -12,6 +12,34 @@ export interface Database {
       [_ in never]: never;
     };
     Tables: {
+      labels: {
+        Insert: {
+          color: string;
+          id?: string;
+          name: string;
+          user_id: string;
+        };
+        Relationships: [
+          {
+            columns: ["user_id"];
+            foreignKeyName: "labels_user_id_fkey";
+            referencedColumns: ["id"];
+            referencedRelation: "profiles";
+          }
+        ];
+        Row: {
+          color: string;
+          id: string;
+          name: string;
+          user_id: string;
+        };
+        Update: {
+          color?: string;
+          id?: string;
+          name?: string;
+          user_id?: string;
+        };
+      };
       memo_notes: {
         Insert: {
           content?: string | null;
@@ -25,7 +53,7 @@ export interface Database {
             columns: ["user_id"];
             foreignKeyName: "memo_notes_user_id_fkey";
             referencedColumns: ["id"];
-            referencedRelation: "users";
+            referencedRelation: "profiles";
           }
         ];
         Row: {
@@ -41,6 +69,37 @@ export interface Database {
           id?: string;
           updated_at?: string | null;
           user_id?: string;
+        };
+      };
+      note_labels: {
+        Insert: {
+          id?: string;
+          label_id: string;
+          note_id: string;
+        };
+        Relationships: [
+          {
+            columns: ["label_id"];
+            foreignKeyName: "note_labels_label_id_fkey";
+            referencedColumns: ["id"];
+            referencedRelation: "labels";
+          },
+          {
+            columns: ["note_id"];
+            foreignKeyName: "note_labels_note_id_fkey";
+            referencedColumns: ["id"];
+            referencedRelation: "memo_notes";
+          }
+        ];
+        Row: {
+          id: string;
+          label_id: string;
+          note_id: string;
+        };
+        Update: {
+          id?: string;
+          label_id?: string;
+          note_id?: string;
         };
       };
       profiles: {
